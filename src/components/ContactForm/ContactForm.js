dce6d3c965850baaba64ee65a3a46d1303a01474
Ivan Rudenko <rudenko.ivan81@gmail.com>
2024-01-15 17:54:54 +0100

    save

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -35,6 +35,7 @@ export const ContactsForm = () => {
           className={css.inputs}
           type="text"
           name="text"
+          placeholder="Name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
@@ -45,6 +46,7 @@ export const ContactsForm = () => {
           className={css.inputs}
           type="tel"
           name="number"
+          placeholder="Phone number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
