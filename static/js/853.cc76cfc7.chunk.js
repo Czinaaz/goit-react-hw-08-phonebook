bb911946ba39c4047fed8722e7b27d466653e21e
Czinaaz <130914798+Czinaaz@users.noreply.github.com>
2024-01-15 13:38:13 +0000

    Deploying to gh-pages from @ Czinaaz/goit-react-hw-08-phonebook@f6e51b5de8d7741511f7dad44011394cb43c17b4 🚀

---
+++ static/js/853.cc76cfc7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[853],{3853:function(t,o,n){n.d(o,{xqh:function(){return r}});var e=n(9983);function r(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"}},{tag:"polyline",attr:{points:"16 17 21 12 16 7"}},{tag:"line",attr:{x1:"21",y1:"12",x2:"9",y2:"12"}}]})(t)}}}]);
+//# sourceMappingURL=853.cc76cfc7.chunk.js.map
